f3230f8b5d3ea5c956e79f34845bdbd28a578ef9
breachcipher <lynxsaiko7@gmail.com>
2026-06-01 13:40:26 +0700

    Update symlink-zram.sh

--- symlink-zram.sh
+++ symlink-zram.sh
@@ -1,10 +1,9 @@
-# Start di runlevel 2,3,4,5 — urutan awal (S02)
-ln -sf /etc/init.d/zram /etc/rc2.d/S02zram
-ln -sf /etc/init.d/zram /etc/rc3.d/S02zram
-ln -sf /etc/init.d/zram /etc/rc4.d/S02zram
-ln -sf /etc/init.d/zram /etc/rc5.d/S02zram
+# Alternatif jika mengikuti struktur murni LFS/BLFS:
+ln -sf /etc/rc.d/init.d/zram /etc/rc.d/rc2.d/S02zram
+ln -sf /etc/rc.d/init.d/zram /etc/rc.d/rc3.d/S02zram
+ln -sf /etc/rc.d/init.d/zram /etc/rc.d/rc4.d/S02zram
+ln -sf /etc/rc.d/init.d/zram /etc/rc.d/rc5.d/S02zram
 
-# Stop di runlevel 0,1,6 — urutan akhir (K98)
-ln -sf /etc/init.d/zram /etc/rc0.d/K98zram
-ln -sf /etc/init.d/zram /etc/rc1.d/K98zram
-ln -sf /etc/init.d/zram /etc/rc6.d/K98zram
+ln -sf /etc/rc.d/init.d/zram /etc/rc.d/rc0.d/K98zram
+ln -sf /etc/rc.d/init.d/zram /etc/rc.d/rc1.d/K98zram
+ln -sf /etc/rc.d/init.d/zram /etc/rc.d/rc6.d/K98zram
